d1ed3c169df4cd1948711cf33249873261fbb581
Lok-ii <lokeshkataria.work@gmail.com>
2024-02-10 22:33:44 +0530

    updated

--- src/Components/Product/ProductPage.jsx
+++ src/Components/Product/ProductPage.jsx
@@ -38,10 +38,9 @@ const ProductPage = memo(() => {
 
   // dispatch(setSingleProduct(productdata));
   // console.log(param.asin);
-
-const apiKey = import.meta.env.MODE === "production" ?  process.env.AMAZON_ASIN : import.meta.env.VITE_AMAZON_ASIN;
-// const apiKey = import.meta.env.VITE_AMAZON_ASIN;
-console.log(apiKey)
+  const apiKey = (import.meta.env.MODE === "production" && process.env.AMAZON_ASIN) ? process.env.AMAZON_ASIN : import.meta.env.VITE_AMAZON_ASIN;
+  console.log("API Key:", apiKey);
+  
   useEffect(() => {
     let i = 0;
     const fetchProductData = async () => {
